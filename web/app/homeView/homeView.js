f5b0d1cde89a76468c03c533dca85b76ecb7ff3a
Marc Jesse <marcjesse92@hotmail.com>
2015-12-02 12:17:31 +0100

    TimeZones now corrected for

--- web/app/homeView/homeView.js
+++ web/app/homeView/homeView.js
@@ -2,28 +2,38 @@
 
 angular.module('myApp.homeView', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/homeView', {
-    templateUrl: 'app/homeView/homeView.html',
-    controller: 'homeViewCtrl',
-    controllerAs : 'ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/homeView', {
+                    templateUrl: 'app/homeView/homeView.html',
+                    controller: 'homeViewCtrl',
+                    controllerAs: 'ctrl'
+                });
+            }])
 
-.controller('homeViewCtrl',function($http, $scope) {
-         
+        .controller('homeViewCtrl', function ($http, $scope) {
+
+
+
+
+            $scope.searchFrom = function () {
+
                 
-          $scope.searchFrom = function(){
-          $http({
-            method: 'GET',
-            url: "api/flightinfo/"+$scope.search.from+"/"+$scope.search.date.toISOString()+"/"+$scope.search.seats
-          }).then(function successCallback(res) {
-            $scope.data = res.data;
-            
-          }, function errorCallback(res) {
-            $scope.error = res.status + ": "+ res.data.statusText;
-          });
-      }
-  
-  
-});
+                var year = $scope.search.date.getFullYear();
+                var month = $scope.search.date.getMonth();
+                var day = $scope.search.date.getDate();
+                var date = new Date(year, month, day, 1);
+
+
+                $http({
+                    method: 'GET',
+                    url: "api/flightinfo/" + $scope.search.from + "/" + date.toISOString() + "/" + $scope.search.seats
+                }).then(function successCallback(res) {
+                    $scope.data = res.data;
+
+                }, function errorCallback(res) {
+                    $scope.error = res.status + ": " + res.data.statusText;
+                });
+            }
+
+
+        });

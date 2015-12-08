2100a54bbee4e1a4f24f4075b0d1fd69a0574890
Zerosource0 <marcjesse92@hotmail.com>
2015-12-08 14:06:53 +0100

    Adding Outsourced Web stuff

--- web/app/app.js
+++ web/app/app.js
@@ -1,27 +1,26 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'ngAnimate',
-  'ui.bootstrap',
-  'myApp.security',
-  'myApp.homeView',
-  'myApp.documentationView',
-  'myApp.companyInfoView',
-  'myApp.reservationsView',
-  'myApp.allUsers',
-  'myApp.newUserView',
-  'myApp.filters',
-  'myApp.directives',
-  'myApp.factories',
-  'myApp.services'
+var myApp = angular.module('myApp', [
+    'ngRoute',
+    'ngAnimate',
+    'ui.bootstrap',
+    'myApp.security',
+    'myApp.homeView',
+    'myApp.documentationView',
+    'myApp.companyInfoView',
+    'myApp.reservationsView',
+    'myApp.allUsers',
+    'myApp.newUserView',
+    'myApp.filters',
+    'myApp.directives',
+    'myApp.factories',
+    'myApp.services'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/homeView'});
-}]).
-config(function ($httpProvider) {
-   $httpProvider.interceptors.push('authInterceptor');
-});
-
-
+        config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.otherwise({redirectTo: '/homeView'});
+            }]).
+        config(function ($httpProvider) {
+            $httpProvider.interceptors.push('authInterceptor');
+        });
+        
---
+++ web/js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+$(document).ready(function () {
+    var divs = $('.mydiv');
+    var dir = 'up'; // wheel scroll direction
+    var div = 0; // current div
+    $(document.body).on('DOMMouseScroll mousewheel', function (e) {
+        if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
+            dir = 'down';
+        } else {
+            dir = 'up';
+        }
+        // find currently visible div :
+        div = -1;
+        divs.each(function(i){
+            if (div<0 && ($(this).offset().top >= $(window).scrollTop())) {
+                div = i;
+            }
+        });
+        if (dir == 'up' && div > 0) {
+            div--;
+        }
+        if (dir == 'down' && div < divs.length) {
+            div++;
+        }
+        //console.log(div, dir, divs.length);
+        $('html,body').stop().animate({
+            scrollTop: divs.eq(div).offset().top
+        }, 200);
+        return false;
+    });
+    $(window).resize(function () {
+        $('html,body').scrollTop(divs.eq(div).offset().top);
+    });
+});
+
+$(document).ready(function(){
+
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
